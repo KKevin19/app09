a66ca75c715fd3076868b0ce483beba8dfa4986f
KKevin19 <rojasocampokevindaniel@cetis50cdmx.com>
2024-10-24 20:01:31 -0600

    Update main.py

--- app09/main.py
+++ app09/main.py
@@ -2,141 +2,132 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    
+#establecer tamaño de pantalla
+
     page.window_width=800
-    page.window_height_800
-
-    page.bgcolor="Black"
-    page.tilte="Mictlan"
-    page.fgcolor="gray"
-
-
-def detener():
-    intro.pause()
-    Nivel1.pause()
-    Nivel2.pause()
-    Nivel3.pause()
-    Nivel4.pause()
-    Nivel5.pause()
-    Nivel6.pause()
-    Nivel7.pause()
-    Nivel8.pause()
-    Nivel9.pause()
-
-
-def playintro():
-    detener()
-    intro.play()
+    page.window_height=800
     
-def playnivel1():    
-    detener()
-    Nivel1.play()
-   
-def playnivel1():    
-    detener()  
-    Nivel2.play()
-
-def playnivel1():    
-    detener()
-    Nivel3.play()
+    page.bgcolor="pink"
+    page.title="mitlan"
+    page.fgcolor="red"
     
-def playnivel1():    
-    detener()    
-    Nivel4.play()
     
-def playnivel1():    
-    detener()    
-    Nivel5.play()
+    #funcion para detener audio
+    def detener():
+            Intro.pause()
+            primernivel.pause()
+            segundonivel.pause()
+            tercernivel.pause()
+            cuartonivel.pause()
+            quintonivel.pause()
+            sextonivel.pause()
+            septimonivel.pause()
+            octavonivel.pause()
+            novenonivel.pause()
+            
+    #funcion  para producir audios
+    def playIntro(e):
+        detener()
+        Intro.play()
+        
+    def playnivel1(e):
+        detener()
+        primernivel.play()
     
-def playnivel1():    
-    detener()    
-    Nivel6.play()
+    def playnivel2(e):
+        detener()
+        segundonivel.play()    
+            
+    def playnivel3(e):
+        detener()
+        tercernivel.play()    
+            
+    def playnivel4(e):
+        detener()
+        cuartonivel.play()    
+            
+    def playnivel5(e):
+        detener()
+        quintonivel.play()    
+            
+    def playnivel6(e):
+        detener()
+        sextonivel.play()   
+        
+    def playnivel7(e):
+        detener()
+        septimonivel.play()   
+        
+    def playnivel8(e):
+        detener()
+        octavonivel.play()   
+        
+    def playnivel9(e):
+        detener()
+        novenonivel.play()   
+            
+    Intro=ft.Audio(src="Intro.mp3",volume=1,balance=0)
+    page.overlay.append(Intro)
     
-def playnivel1():    
-    detener()    
-    Nivel7.play()
-
-def playnivel1():    
-    detener()
-    Nivel8.play()
+    primernivel=ft.Audio(src="Primer_nivel.mp3",volume=1,balance=0)
+    page.overlay.append(primernivel)
     
-def playnivel1():    
-    detener()    
-    Nivel9.play()
+    segundonivel=ft.Audio(src="segundo_nivel.mp3",volume=1,balance=0)
+    page.overlay.append(segundonivel)
+    
+    tercernivel=ft.Audio(src="tercer_nivel.mp3",volume=1,balance=0)
+    page.overlay.append(tercernivel)
+    
+    cuartonivel=ft.Audio(src="cuarto_nivel.mp3",volume=1,balance=0)
+    page.overlay.append(cuartonivel)
+    
+    quintonivel=ft.Audio(src="quinto_nivel.mp3",volume=1,balance=0)
+    page.overlay.append(quintonivel)
+    
+    sextonivel=ft.Audio(src="sexto_nivel.mp3",volume=1,balance=0)
+    page.overlay.append(sextonivel)
+    
+    septimonivel=ft.Audio(src="septimo_nivel.mp3",volume=1,balance=0)
+    page.overlay.append(septimonivel)
+    
+    octavonivel=ft.Audio(src="octavo_nivel.mp3",volume=1,balance=0)
+    page.overlay.append(octavonivel)
+    
+    novenonivel=ft.Audio(src="noveno_nivel.mp3",volume=1,balance=0)
+    page.overlay.append(novenonivel)
+    
+#se crea la interfaz
 
-    intro=ft.Audio(src="Intro.mp3",volume=1,balance=0)
-    page.overlay.append(intro)
+    btnIntro=ft.FilledButton(text="escucha el Intro", disabled=False,on_click=playIntro)
+    
+    btnnivel1=ft.ElevatedButton(text="primer nivel",on_click=playnivel1)
+    img1=ft.Image(src="primer-nivel.jpeg",width=150,height=150)
+    
+    btnnivel2=ft.ElevatedButton(text="segundo nivel",on_click=playnivel2)
+    img2=ft.Image(src="segundo-nivel.jpeg",width=150,height=150)
 
-    Nivel1=ft.Audio(src="Primer_Nivel.mp3",volume=1,balance=0)
-    page.overlay.append(Nivel1)
+    btnnivel3=ft.ElevatedButton(text="tercer nivel",on_click=playnivel3)
+    img3=ft.Image(src="tercer-nivel.png",width=150,height=150)
 
-    Nivel2=ft.Audio(src="Segundo_Nivel.mp3",volume=1,balance=0)
-    page.overlay.append(Nivel2)
+    btnnivel4=ft.ElevatedButton(text="cuarto nivel",on_click=playnivel4)
+    img4=ft.Image(src="cuarto-nivel.jpeg",width=150,height=150)
 
-    Nivel3=ft.Audio(src="Tercer_Nivel.mp3",volume=1,balance=0)
-    page.overlay.append(Nivel3)
+    btnnivel5=ft.ElevatedButton(text="quinto nivel",on_click=playnivel5)
+    img5=ft.Image(src="quinto-nivel.jpeg",width=150,height=150)
 
-    Nivel4=ft.Audio(src="Cuarto_Nivel.mp3",volume=1,balance=0)
-    page.overlay.append(Nivel4)
+    btnnivel6=ft.ElevatedButton(text="sexto nivel",on_click=playnivel6)
+    img6=ft.Image(src="sexto-nivel.jpeg",width=150,height=150)
 
-    Nivel5=ft.Audio(src="Quinto_Nivel.mp3",volume=1,balance=0)
-    page.overlay.append(Nivel5)
+    btnnivel7=ft.ElevatedButton(text="septimo nivel",on_click=playnivel7)
+    img7=ft.Image(src="septimo-nivel.jpeg",width=150,height=150)
 
-    Nivel6=ft.Audio(src="Sexto_Nivel.mp3",volume=1,balance=0)
-    page.overlay.append(Nivel6)
+    btnnivel8=ft.ElevatedButton(text="octavo nivel",on_click=playnivel8)
+    img8=ft.Image(src="octavo-nivel.png",width=150,height=150)
 
-    Nivel7=ft.Audio(src="Septimo_Nivel.mp3",volume=1,balance=0)
-    page.overlay.append(Nivel7)
-
-    Nivel8=ft.Audio(src="Octavo_Nivel.mp3",volume=1,balance=0)
-    page.overlay.append(Nivel8)
-
-    Nivel9=ft.Audio(src="Noveno_Nivel.mp3",volume=1,balance=0)
-    page.overlay.append(Nivel9)
-
-
-
-    btnIntro=ft.FilledButton(text="Escucha el intro",disabled=False)
+    btnnivel9=ft.ElevatedButton(text="noveno nivel",on_click=playnivel9)
+    img9=ft.Image(src="noveno-nivel.jpeg",width=150,height=150)
 
     
-    btnNivel1=ft.ElevatedButton(text="Primer Nivel"),
-   
-    img1=ft.Image(src="Primer-Nivel.jpeg",width=150,height=150)
-
-    btnNivel2=ft.ElevatedButton(text="Segundo Nivel"),
-   
-    img2=ft.Image(src="Segundo-Nivel.jpeg",width=150,height=150)
-
-    btnNivel3=ft.ElevatedButton(text="Terser Nivel"),
-   
-    img3=ft.Image(src="Terser-Nivel.png",width=150,height=150)
-
-    btnNivel4=ft.ElevatedButton(text="Cuarto Nivel"),
-   
-    img4=ft.Image(src="Cuarto-Nivel.jpeg",width=150,height=150)
-
-    btnNivel5=ft.ElevatedButton(text="Quinto Nivel"),
-   
-    img5=ft.Image(src="Quinto-Nivel.jpeg",width=150,height=150)
-
-    btnNivel6=ft.ElevatedButton(text="Sexto Nivel"),
-   
-    img6=ft.Image(src="Sexto-Nivel.jpeg",width=150,height=150)
-
-    btnNivel7=ft.ElevatedButton(text="Septimo Nivel"),
-   
-    img7=ft.Image(src="Septimo-Nivel.jpeg",width=150,height=150)
-
-    btnNivel8=ft.ElevatedButton(text="Octavo Nivel"),
-
-    img8=ft.Image(src="Octavo-Nivel.png",width=150,height=150)
-   
-    btnNivel9=ft.ElevatedButton(text="Primer Nivel"),
-   
-    img9=ft.Image(src="Primer-Nivel.jpeg",width=150,height=150)
-
-
-
     page.add(
         ft.Row(
             alignment="start",
@@ -153,37 +144,17 @@ def playnivel1():
                         ft.Column(
                             alignment="CENTER",
                             spacing=10,
-                            controls=[btnNivel1,img1]
+                            controls=[btnnivel1,img1]
                         ),
                         ft.Column(
                             alignment="CENTER",
-                            controls=[btnNivel2,img2]
+                            controls=[btnnivel2,img2]
                         ),
                         ft.Column(
                             alignment="CENTER",
-                            controls=[btnNivel3,img3]
+                            controls=[btnnivel3,img3]
                     )
                 ]
-            )
-        ]
-    )
-    ft.Row(
-        alignment="CENTER",
-        controls=[
-            ft.Column(
-                alignment="CENTER",
-                spacing=10,
-                controls=[btnNivel4,img4]
-            ),
-                ft.Column(
-                alignment="CENTER",
-                spacing=10,
-                controls=[btnNivel5,img5]  
-            ),
-            ft.Column(
-                alignment="CENTER",
-                spacing=10,
-                controls=[btnNivel6,img6]
             )
         ]
     ),
@@ -193,17 +164,40 @@ def playnivel1():
             ft.Column(
                 alignment="CENTER",
                 spacing=10,
-                controls=[btnNivel7,img7]
+                controls=[btnnivel4,img4]
+            ),
+                ft.Column(
+                alignment="CENTER",
+                spacing=10,
+                controls=[btnnivel5,img5]  
             ),
             ft.Column(
                 alignment="CENTER",
                 spacing=10,
-                controls=[btnNivel8,img8]  
+                controls=[btnnivel6,img6]
+            )
+        ]
+    ),
+    ft.Row(
+        alignment="CENTER",
+        controls=[
+            ft.Column(
+                alignment="CENTER",
+                spacing=10,
+                controls=[btnnivel7,img7]
             ),
             ft.Column(
-                alignment="CENTER",                   spacing=10
-                controls=[btnNivel9,img9]
+                alignment="CENTER",
+                spacing=10,
+                controls=[btnnivel8,img8]  
+            ),
+            ft.Column(
+                alignment="CENTER",                   
+                spacing=10,
+                controls=[btnnivel9,img9]
             )
         ]
     )
 )
+
+    ft.app(main)
